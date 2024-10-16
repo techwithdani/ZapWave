58e2c7f1d0f7ac3efd0fd95a33d5e2159dcf0953
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-10-16 08:17:51 +0500

    Configure userSchema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: () => Date.now(), immutable: true },
+  updatedAt: Date,
+});
 
 const User = mongoose.model("User", userSchema);
 
