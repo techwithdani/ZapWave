9e87a5d9ea0f720bb25d7188d4e82c3ec047c177
Muhammad Daniyal <154412066+techwithdani@users.noreply.github.com>
2024-10-16 08:05:56 +0500

    Create schema and model with mongoose + Add dependencies + Temporarily remove sockets + Create Connection with the database

--- server/index.js
+++ server/index.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const { createServer } = require("node:http");
-const { Server } = require("socket.io");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
 
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
-io.on("connection", (socket) => {
-    console.log("A User Connected...");
-});
+mongoose
+  .connect(process.env.CONNECTION_STRING, { dbName: "UserData" })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => console.log(error));
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server running at http://localhost:${process.env.PORT}`);
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
